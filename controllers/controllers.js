7756c86c987e6a807e96266f357966ed0745360d
Kodi <30986906+kodiwalker@users.noreply.github.com>
2023-07-07 16:02:59 -0600

    Implemented express validator

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,4 +1,3 @@
-//db connection
 const { Translate } = require('@google-cloud/translate').v2;
 const translate = new Translate();
 const axios = require('axios');
@@ -6,24 +5,21 @@ let lastTranslatedText;
 const countries = require('countries-list');
 const countryTimezones = require('countries-and-timezones');
 const moment = require('moment-timezone');
+const { body, validationResult } = require('express-validator');
 
 const countryMappings = {};
 
 for (const countryCode in countries.countries) {
   const country = countries.countries[countryCode];
-
-  // Select the first language and currency
   const firstLanguage = country.languages[0]
   const currency = country.currency.length > 3 ? country.currency.split(',')[0] : country.currency;
-
-  // Get all timezones for this country
   const timezones = countryTimezones.getTimezonesForCountry(countryCode);
 
   countryMappings[countryCode] = {
     name: country.name,
-    language: firstLanguage, // First ISO-639-1 code
-    currency: currency, // ISO-4217 code
-    timezones: timezones // All timezones for the country
+    language: firstLanguage,
+    currency: currency,
+    timezones: timezones
   };
 }
 
@@ -31,63 +27,82 @@ exports.getCountries = async (req, res) => {
   res.json(countryMappings);
 }
 
-exports.getTime = async (req, res) => {
-  const { home, away } = req.body;
-  console.log('-----____---______---', home, away)
-  async function getLocalTime(timezone) {
-    const response = await axios.get(`http://worldtimeapi.org/api/timezone/${timezone}`);
+exports.getTime = [
+  body('home').trim().escape(),
+  body('away').trim().escape(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-    const unixTime = response.data.unixtime;
-    const localTime = moment.unix(unixTime).tz(timezone).format('YYYY-MM-DDTHH:mm:ss');
+    const { home, away } = req.body;
+    async function getLocalTime(timezone) {
+      const response = await axios.get(`http://worldtimeapi.org/api/timezone/${timezone}`);
+      const unixTime = response.data.unixtime;
+      const localTime = moment.unix(unixTime).tz(timezone).format('YYYY-MM-DDTHH:mm:ss');
+      return localTime;
+    }
 
-    return localTime;
+    try {
+      const homeTime = await getLocalTime(home);
+      const awayTime = await getLocalTime(away);
+      res.json({ homeTime, awayTime });
+    } catch (e) {
+      console.error('Failed to get times:', e.error);
+      res.status(500).json({ error: 'Failed to get times.' });
+    }
   }
+];
 
+exports.translate = [
+  body('text').trim().escape(),
+  body('target').trim().escape(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  try {
-    const homeTime = await getLocalTime(home);
-    const awayTime = await getLocalTime(away);
+    const { text, target } = req.body;
+    if (text.trim() === '' || text === lastTranslatedText || text.trim() === lastTranslatedText) {
+      lastTranslatedText = text;
+      res.sendStatus(400);
+      return;
+    }
 
-    res.json({ homeTime, awayTime });
-  } catch (e) {
-    console.error('Failed to get times:', e.error);
-    res.status(500).json({ error: 'Failed to get times.' });
-  }
-}
-
-exports.translate = async (req, res) => {
-  const { text, target } = req.body;
-  if (text.trim() === '' || text === lastTranslatedText || text.trim() === lastTranslatedText) {
     lastTranslatedText = text;
-    res.sendStatus(400);
-    return;
+    try {
+      const [translation] = await translate.translate(text, target);
+      res.json(translation);
+    } catch (e) {
+      console.error('Failed to translate:', e)
+      res.status(500).json({ error: 'Translate failed...' })
+    }
   }
+];
 
-  lastTranslatedText = text;
+exports.convert = [
+  body('home').trim().escape(),
+  body('away').trim().escape(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  try {
-    const [translation] = await translate.translate(text, target);
-    res.json(translation);
-  } catch (e) {
-    console.error('Failed to translate:', e)
-    res.status(500).json({ error: 'Translate failed...' })
+    const { home, away } = req.body;
+    try {
+      const [homeRateData, awayRateData] = await Promise.all([
+        axios.get(`https://v6.exchangerate-api.com/v6/44a819715fd986bd0cf9ad89/enriched/${home}/${away}`).then(res => res.data),
+        axios.get(`https://v6.exchangerate-api.com/v6/44a819715fd986bd0cf9ad89/enriched/${away}/${home}`).then(res => res.data)
+      ]);
+      res.json({ homeRateData, awayRateData });
+    } catch (e) {
+      console.error('Failed to get conversion rates:', e)
+      res.status(500).json({ error: 'Failed to get conversion rates...' })
+    }
   }
-}
-
-exports.convert = async (req, res) => {
-  const { home, away } = req.body;
-
-  try {
-    const [homeRateData, awayRateData] = await Promise.all([
-      axios.get(`https://v6.exchangerate-api.com/v6/44a819715fd986bd0cf9ad89/enriched/${home}/${away}`).then(res => res.data),
-      axios.get(`https://v6.exchangerate-api.com/v6/44a819715fd986bd0cf9ad89/enriched/${away}/${home}`).then(res => res.data)
-    ]);
-
-    res.json({ homeRateData, awayRateData });
-  } catch (e) {
-    console.error('Failed to get conversion rates:', e)
-    res.status(500).json({ error: 'Failed to get conversion rates...' })
-  }
-}
+];
 
 module.exports = exports;
